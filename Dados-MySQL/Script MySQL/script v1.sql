fc9e2d1bb9fa8f250a61a6ad27c1bb5eaea93e07
matheusfis <matheus.fischer@sptech.school>
2024-09-29 20:11:26 -0300

    Mudança no nome do id da tabela disciplina

--- Dados-MySQL/Script MySQL/script v1.sql
+++ Dados-MySQL/Script MySQL/script v1.sql
@@ -3,23 +3,23 @@ use dbnexus;
 -- Criação da tabela aluno
 CREATE TABLE IF NOT EXISTS aluno (
     idAluno INT AUTO_INCREMENT PRIMARY KEY,
-    nome_instituicao VARCHAR(45) NOT NULL,
+	instituicao VARCHAR(45) NOT NULL,
     serie VARCHAR(45) NOT NULL,
     periodo VARCHAR(45) NOT NULL
 );
 
 -- Criação da tabela disciplina
 CREATE TABLE IF NOT EXISTS disciplina (
-    idDisc INT AUTO_INCREMENT PRIMARY KEY,
-    nome_disciplina VARCHAR(45) UNIQUE NOT NULL 
+    idDisciplina INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(45) UNIQUE NOT NULL 
 );
 
 -- Criação da tabela notas_aluno
 CREATE TABLE IF NOT EXISTS notas_aluno (
     fkAluno INT,
-    fkDisc INT,
+    fkDisciplina INT,
     nota VARCHAR(45) NOT NULL,
     PRIMARY KEY (fkAluno, fkDisc),
     FOREIGN KEY (fkAluno) REFERENCES aluno(idAluno),
-    FOREIGN KEY (fkDisc) REFERENCES disciplina(idDisc)
+    FOREIGN KEY (idDisciplina) REFERENCES disciplina(idDisciplina)
 );
