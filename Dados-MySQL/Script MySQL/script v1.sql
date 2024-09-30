51244e5e026c1663a5430ed800050278092592fe
matheusfis <matheus.fischer@sptech.school>
2024-09-30 19:47:16 -0300

    Alteração script mysql

--- Dados-MySQL/Script MySQL/script v1.sql
+++ Dados-MySQL/Script MySQL/script v1.sql
@@ -2,8 +2,13 @@ CREATE DATABASE dbnexus;
 
 USE dbnexus;
 
+DROP TABLE IF EXISTS instituicao;
+DROP TABLE IF EXISTS turma;
+DROP TABLE IF EXISTS disciplina;
+DROP TABLE IF EXISTS notas_turma;
+
 -- Criação da tabela 'instituicao'
-CREATE TABLE instituicao (
+CREATE TABLE IF NOT EXISTS instituicao (
     idInstituicao INT AUTO_INCREMENT PRIMARY KEY,
     nome_instituicao VARCHAR(45) NOT NULL,
     nome_departamento VARCHAR(45),
@@ -12,20 +17,20 @@ CREATE TABLE instituicao (
 );
 
 -- Criação da tabela 'turma'
-CREATE TABLE turma (
+CREATE TABLE IF NOT EXISTS turma (
     idTurma INT AUTO_INCREMENT PRIMARY KEY,
     serie VARCHAR(45),
     periodo VARCHAR(45)
 );
 
 -- Criação da tabela 'disciplina'
-CREATE TABLE disciplina (
+CREATE TABLE IF NOT EXISTS disciplina (
     idDisc INT AUTO_INCREMENT PRIMARY KEY,
     nome_disciplina VARCHAR(45)
 );
 
 -- Criação da tabela 'notas_turma'
-CREATE TABLE notas_turma (
+CREATE TABLE IF NOT EXISTS  notas_turma (
     dkAluno INT,
     fkDisc INT,
     media VARCHAR(45),
